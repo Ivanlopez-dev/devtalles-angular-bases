65975432f89b81b194588bec724b4f39583b0aea
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-10 16:36:18 +0200

    add: ➰ for loop

---
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+
+export interface Character {
+  id: number
+  name: string
+  power: number
+}
+
+@Component({
+  templateUrl: './dragonball-page.html'
+})
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 1, name: 'Vegetta', power: 8000 },
+    { id: 1, name: 'Piccolo', power: 9000 },
+  ])
+}
