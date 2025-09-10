e953e21e9fc7c0f66be59ce50a492ff616e3142b
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-10 17:06:13 +0200

    add: 💭 if else condition

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -13,8 +13,9 @@ export interface Character {
 export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 1, name: 'Vegetta', power: 8000 },
-    { id: 1, name: 'Piccolo', power: 3000 },
+    { id: 2, name: 'Vegetta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ])
 
   // powerClasses = computed(() => {
