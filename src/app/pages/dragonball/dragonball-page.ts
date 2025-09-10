fce9997c009117576c3054652d5b7dd803a295af
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-10 16:54:40 +0200

    add: 📝 conditional classes

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -7,12 +7,19 @@ export interface Character {
 }
 
 @Component({
-  templateUrl: './dragonball-page.html'
+  templateUrl: './dragonball-page.html',
+  // imports: [NgClass]
 })
 export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 1, name: 'Vegetta', power: 8000 },
-    { id: 1, name: 'Piccolo', power: 9000 },
+    { id: 1, name: 'Piccolo', power: 3000 },
   ])
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true,
+  //   }
+  // })
 }
