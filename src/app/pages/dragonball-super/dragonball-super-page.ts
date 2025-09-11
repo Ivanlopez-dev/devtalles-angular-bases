964dad157c53bf158e2baa54e0c64338e3e16349
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-11 12:28:07 +0200

    add: ✏️ independent service/logic

--- src/app/pages/dragonball-super/dragonball-super-page.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.ts
@@ -1,12 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { CharacterListComponent } from "../../components/dragonball/character-list/character-list";
-import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add";
-
-export interface Character {
-  id: number
-  name: string
-  power: number
-}
+import { Component, inject } from '@angular/core'
+import { CharacterListComponent } from "../../components/dragonball/character-list/character-list"
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add"
+import { DragonballService } from '../../services/dragonball.service'
 
 @Component({
   templateUrl: './dragonball-super-page.html',
@@ -14,20 +9,6 @@ export interface Character {
   imports: [CharacterListComponent, CharacterAddComponent]
 })
 export class DragonballSuperPageComponent {
-  name = signal('')
-  power = signal(0)
+  public dragonballService = inject(DragonballService)
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegetta', power: 8000 },
-  ])
-
-  addCharacter(character: Character) {
-    this.characters.update((list) => [...list, character])
-  }
-
-  resetFields() {
-    this.name.set('')
-    this.power.set(0)
-  }
 }
