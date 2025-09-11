297574dfa18553b4ae4e882cce2c113fc52cfb5c
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-11 12:55:55 +0200

    add: 🔃 load from local storage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,12 +1,14 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters')
+  return characters ? JSON.parse(characters) : []
+}
+
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegetta', power: 8000 },
-  ])
+  characters = signal<Character[]>(loadFromLocalStorage())
 
   addCharacter(character: Character) {
     this.characters.update((list) => [...list, character])
@@ -15,6 +17,4 @@ export class DragonballService {
   saveToLocalStorage = effect(() => {
     localStorage.setItem('characters', JSON.stringify(this.characters()))
   })
-
 }
-
