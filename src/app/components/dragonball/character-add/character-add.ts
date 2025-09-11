3d1856ce83ca5ee0b20d4ef4857c90238f34027a
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-11 11:56:32 +0200

    add: ✈️ output - emitir valores

--- src/app/components/dragonball/character-add/character-add.ts
+++ src/app/components/dragonball/character-add/character-add.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { Character } from '../../../interfaces/character.interface';
 
 @Component({
@@ -8,6 +8,8 @@ import { Character } from '../../../interfaces/character.interface';
 export class CharacterAddComponent{
   name = signal('')
   power = signal(0)
+
+  newCharacter = output<Character>()
 
   addCharacter() {
       if (!this.name() || !this.power() || this.power() <= 0) {
@@ -21,8 +23,7 @@ export class CharacterAddComponent{
       }
 
       // this.characters.update((list) => [...list, newCharacter])
-      console.log({ newCharacter });
-
+      this.newCharacter.emit(newCharacter)
       this.resetFields()
     }
 
