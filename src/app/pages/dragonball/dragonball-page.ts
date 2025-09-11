74e18b3dcf44f6394c05116029f349899d4b9e45
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-11 10:28:11 +0200

    add: ✏️ inputs - how to work with

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -8,9 +8,12 @@ export interface Character {
 
 @Component({
   templateUrl: './dragonball-page.html',
-  // imports: [NgClass]
+
 })
 export class DragonballPageComponent {
+  name = signal('Gohan')
+  power = signal(100)
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegetta', power: 8000 },
@@ -18,9 +21,23 @@ export class DragonballPageComponent {
     { id: 4, name: 'Yamcha', power: 500 },
   ])
 
-  // powerClasses = computed(() => {
-  //   return {
-  //     'text-danger': true,
-  //   }
-  // })
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1 ,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update((list) => [...list, newCharacter])
+    this.resetFields()
+  }
+
+  resetFields() {
+    this.name.set('')
+    this.power.set(0)
+  }
 }
